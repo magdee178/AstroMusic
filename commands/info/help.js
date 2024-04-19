5b8ab4f82a0af9f1c3f1cbce85c56e61c069b2ba
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-19 02:13:45 +0200

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -16,29 +16,6 @@ module.exports = {
 
             let embed = new EmbedBuilder()
             .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
-            /*
-                            .addFields(
-                                { name: `${prefix}247`, value: `Toggles the 24/7 mode. This makes the bot doesn't leave the voice channel until you stop it.`, inline: false },
-                                { name: `${prefix}autoplay`, value: `Toggles autoplay for the current guild.`, inline: false },
-                                { name: `${prefix}join`, value: `join the voice channel.`, inline: false },
-                                { name: `${prefix}jump`, value: `Skip to a song in the queue.`, inline: false },
-                                { name: `${prefix}leave`, value: `leave the voice channel.`, inline: false },
-                                { name: `${prefix}loop`, value: `Toggles the repeat mode.`, inline: false },
-                                { name: `${prefix}lyrics`, value: `Display lyrics of a song`, inline: false },
-                                { name: `${prefix}nowplaying`, value: `Shows what is song that the bot is currently playing.`, inline: false },
-                                { name: `${prefix}pause`, value: `Pauses the currently playing track.`, inline: false },
-                                { name: `${prefix}play`, value: `Add a song to queue and plays it.`, inline: false },
-                                { name: `${prefix}previous`, value: `Plays the previous song in the queue.`, inline: false },
-                                { name: `${prefix}queue`, value: `Display the queue of the current tracks in the playlist.`, inline: false },
-                                { name: `${prefix}resume`, value: `Resumes the currently paused track.`, inline: false },
-                                { name: `${prefix}search`, value: `Search song and play music.`, inline: false },
-                                { name: `${prefix}seek`, value: `Seeks to a certain point in the current track.`, inline: false },
-                                { name: `${prefix}shuffle`, value: `Shuffle the queue.`, inline: false },
-                                { name: `${prefix}skip`, value: `Skip the current song.`, inline: false },
-                                { name: `${prefix}stop`, value: `Stop the current song and clears the entire music queue.`, inline: false },
-                                { name: `${prefix}volume`, value: `Changes/Shows the current volume.`, inline: false },
-                                )
-            */
             commandFiles.map((value) => {
                 const file = require(value);
                 const splitted = value.split("/");
@@ -46,12 +23,15 @@ module.exports = {
 
                 if (file.name) {
                     const properties = { directory, ...file };
-                    embed.addFields({ name: `${prefix}${properties.name}`, value: `${properties.description}`, inline: false })
+                  
+                  embed.setAuthor({ name: `${message.guild.name}`, iconURL: message.guild.iconURL({ dynamic: true })})
+                  embed.setDescription(`**__Prefix:__** \`${prefix}\`\n**__Commands:__** \`${client.commands.size}\`\n**__Slash:__** \`${client.slashCommands.size}\`\n**__Developer:__** [𝐌𝐚𝐆𝐝𝐄𝐞](https://discord.com/users/1150890847768936458)\n`)
+                    embed.addFields({ name: `<:line_ma:1229863001058840708> ${prefix}${properties.name} (${properties.aliases})` , value: `------------------`, inline: false})
                 }
             });
 
-            let row = new ActionRowBuilder()
-                .addComponents(
+            //let row = new ActionRowBuilder()
+                /*.addComponents(
                     new ButtonBuilder()
                         .setStyle(ButtonStyle.Link)
                         .setLabel('Invite Bot')
@@ -61,9 +41,10 @@ module.exports = {
                     new ButtonBuilder()
                         .setStyle(ButtonStyle.Link)
                         .setLabel('Server Support')
-                        .setURL(`https://discord.gg/developer-tools`))
+                        .setURL(`https://discord.gg/developer-tools`)) */
+          embed.setFooter({ text: `Requested by:  ${message.guild.members.cache.get(message.author.id).displayName}`, iconURL: message.author.displayAvatarURL({ dynamic: true })});
 
-            message.reply({ embeds: [embed], components: [row] })
+            message.reply({ embeds: [embed] })
         } catch (err) {
             console.log(err)
         }
