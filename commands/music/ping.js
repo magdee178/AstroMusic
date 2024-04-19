6ca8e1000a7d320b666804570551964f50f15aaa
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-19 02:18:39 +0200

    Create ping.js

---
+++ commands/music/ping.js
@@ -0,0 +1,17 @@
+const { EmbedBuilder } = require("discord.js");
+const { Utils } = require("devtools-ts");
+const utilites = new Utils();
+
+module.exports = {
+    name: "ping",
+    description: `Test the bots response time.`,
+  aliases: ['pi'],
+    cooldown: 5000,
+    async execute(client, message, args) {
+        try {
+            message.reply({ content: `:ping_pong: Pong ${client.ws.ping} ms` })
+        } catch (err) {
+            console.log(err)
+        }
+    },
+};
